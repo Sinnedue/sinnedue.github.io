b21b763157e5bf82f2ac191ab0ecbe9981b756c6
Sinnedue <53600498+Sinnedue@users.noreply.github.com>
2022-11-03 04:48:47 -0700

    Changes to index.html
    * Removed ytplayer.php
    * Adjusted autoplayer

--- ytplayer.php
+++
@@ -1,5 +0,0 @@
-<html>
-<body leftmargin="0" topmargin="0" marginwidth="0" marginheight="0">
-<iframe width="100" height="300" src="https://www.youtube.com/embed/videoseries?list=PL31TuEfBY-qBoIGTKdEOohvGW5pLSbUDC&autoplay=0&index=8&loop=1" frameborder="0" gesture="media" allow="encrypted-media"></iframe>
-</body>
-</html>
